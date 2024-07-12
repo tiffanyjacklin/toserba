0c26bb6b9bb4a2588b0f52901a7cb798ba1949e6
c14210056 <tiffanyjacklin29@gmail.com>
2024-07-12 20:49:33 +0700

    7/12/2024

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -55,7 +55,7 @@
 			</div>
 			</div>
 			<button type="submit" class="inline-flex items-center rounded-md bg-f2b082 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-				<a href="/about">Login</a>
+				<a href="/components/Home.svelte">Login</a>
 				 
 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 					<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
