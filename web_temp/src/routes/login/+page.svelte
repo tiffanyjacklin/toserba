bbfec3c77e33fc1a7cb8ca53225d240d4d1e913b
c14210056 <tiffanyjacklin29@gmail.com>
2024-07-29 10:14:17 +0700

    bg login

--- web_temp/src/routes/login/+page.svelte
+++ web_temp/src/routes/login/+page.svelte
@@ -52,7 +52,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-[2px] absolute h-5 w-5 top-2 left-3 text-[#3d4c52]">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                         </svg>
-                        <input id="username" name="username" type="text" placeholder="Username" class="shadow-lg focus:ring-2 focus:ring-orange-300 focus:outline-none focus:border-orange-300 appearance-none block w-full px-10 py-2 text-sm leading-6 text-slate-900 rounded-md  " required>
+                        <input id="username" name="username" type="text" placeholder="Username" class="shadow-lg focus:ring-2 focus:ring-orange-300 focus:outline-none focus:border-0 appearance-none block w-full px-10 py-2 text-sm leading-6 text-slate-900 rounded-md  " required>
                     </div>
                 </div>
                 <div class="w-full">
@@ -61,7 +61,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="stroke-[2px] absolute h-5 w-5 top-2 left-3 text-[#3d4c52]">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
                         </svg>
-                        <input id="password" name="password" type="password" placeholder="Password" class="shadow-lg focus:ring-2 focus:ring-orange-300 focus:outline-none focus:border-orange-300 appearance-none block w-full px-10 py-2 text-sm leading-6 text-slate-900 rounded-md" required>
+                        <input id="password" name="password" type="password" placeholder="Password" class="shadow-lg focus:ring-2 focus:ring-orange-300 focus:outline-none focus:border-0 appearance-none block w-full px-10 py-2 text-sm leading-6 text-slate-900 rounded-md" required>
                     </div>
                 </div>
                 <div>
@@ -80,6 +80,13 @@
         </div>
     </figure>
 </div>
+<style lang="postcss">
+	#cover{
+		background: linear-gradient(45deg, #3d4c52 0%, #3d4c52 44%, #f7d4b2 44%, #f7d4b2 100%);
+	}	
+</style>
+
+
 <!-- 
 <style lang="postcss">
 	@tailwind components;
