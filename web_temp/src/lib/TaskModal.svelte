28a01a8ddde1612d0c0489ac97cb5045c2ba60db
c14210056 <tiffanyjacklin29@gmail.com>
2024-08-06 15:11:25 +0700

    modal

--- web_temp/src/lib/TaskModal.svelte
+++ web_temp/src/lib/TaskModal.svelte
@@ -16,9 +16,9 @@
 
 {#if open}
      <div class="overlay" role="dialog" aria-labelledby="modal-title" on:click={close} on:keydown={handleKeyDown}>
-        <div class="modal-content bg-[{color}]" on:click|stopPropagation >
+        <div class="modal-content bg-[{color}] shadow-[0_6px_0px_0px_rgba(242,176,130,1)]" on:click|stopPropagation >
             <!-- <button class="close-button" type="button" on:click={close}>✖</button> -->
-            <button type="button" on:click={close} class="close-button text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+            <button type="button" on:click={close} class="close-button text-gray-400 bg-transparent hover:bg-gray-200 hover:text-white rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                 </svg>
@@ -54,8 +54,8 @@
 
     .close-button {
         position: absolute;
-        top: 10px;
-        right: 10px;
+        top: 32px;
+        right: 32px;
         border: none;
         background: transparent;
         font-size: 24px;
