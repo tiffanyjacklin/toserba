d6aef8199ca3ef06d15fdecf43e9532416a10086
tiffanyjacklin <tiffanyjacklin29@gmail.com>
2024-09-18 15:28:54 +0700

    datenow.js

--- web_temp/src/lib/DateNow.js
+++ web_temp/src/lib/DateNow.js
@@ -9,10 +9,8 @@ export function getFormattedDate() {
 
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
-export function getThirtyDaysBefore() {
-  const today = new Date();
-  const now = new Date(today);
-  now.setDate(today.getDate() - 30);
+export function getFormattedDateNow() {
+  const now = new Date();
   const year = now.getFullYear();
   const month = String(now.getMonth() + 1).padStart(2, '0');
   const day = String(now.getDate()).padStart(2, '0');
@@ -20,7 +18,20 @@ export function getThirtyDaysBefore() {
   const minutes = String(now.getMinutes()).padStart(2, '0');
   const seconds = String(now.getSeconds()).padStart(2, '0');
 
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return `${year}-${month}-${day}`;
+}
+export function getThirtyDaysBefore(date, sum) {
+  const today = new Date(date);
+  const now = new Date(today);
+  now.setDate(today.getDate() - sum);
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
 }
 
 export function isInTimeRange(closingTime, rangeMinutes = 30) {
